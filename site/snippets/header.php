cd4b0f0a276a31a26cae9d7c91fc15d932389dc2
Loïc Horellou <hello@loichorellou.net>
2019-04-22 17:33:38 +0200

    update 1600px layout

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,15 +5,17 @@
 <meta charset="utf-8">
 <!-- <meta name="viewport" content="width=device-width,initial-scale=1.0"> -->
 <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+<meta name='rb:domain' content='<?= parse_url( $kirby->url() )['host'] ?>'>
 
 <title><?= $site->title() ?> | <?= $page->title() ?></title>
 
 <?= js(['assets/vendor/jquery-3.3.1.min.js',
-		 'assets/vendor/jquery.cookie-1.4.1.min.js',
+		 'assets/vendor/js.cookie-2.2.0.min.js',
+		 // 'assets/vendor/jquery.cookie-1.4.1.min.js',
 		 'assets/vendor/jssocials-1.4.0/dist/jssocials.min.js',
 		 'assets/vendor/slick-1.8.1/slick/slick.min.js',
 		 'https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.js',
-		 'assets/js/main.js?v=1.26']) ?> 
+		 'assets/js/main.js?v=1.30']) ?> 
 
 <?= css(['https://use.typekit.net/vrl2tmu.css',
 		 'https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css',
@@ -21,7 +23,7 @@
 		 'assets/vendor/jssocials-1.4.0/dist/jssocials.css',
 		 'assets/vendor/slick-1.8.1/slick/slick.css',
 		 'assets/css/reset.css',
-		 'assets/css/style.css?v=1.20'/*,
+		 'assets/css/style.css?v=1.21'/*,
 		 '@auto'*/]) ?>
 
 <?php
